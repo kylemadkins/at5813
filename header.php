36577d71e48dba8775a18fbc86f7afba41e2ee14
Kyle Adkins <kyle.m.adkins@gmail.com>
2021-09-02 22:56:37 -0500

    Style home page and add content

--- header.php
+++ header.php
@@ -16,6 +16,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@400;700&family=Work+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
@@ -26,17 +29,17 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'at5813' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
+		<!-- <div class="site-branding">
 			<div class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
-		</div>
+		</div> -->
 
-		<nav id="site-navigation" class="main-navigation">
+		<!-- <nav id="site-navigation" class="main-navigation">
 			<?php
-			wp_nav_menu(
-				array(
-					'menu' => 'menu'
-				)
-			);
+			// wp_nav_menu(
+			// 	array(
+			// 		'menu' => 'menu'
+			// 	)
+			// );
 			?>
-		</nav>
+		</nav> -->
 	</header>
--- home-page.php
+++ home-page.php
@@ -6,3 +6,17 @@ Template Post Type: page
 get_header();
 ?>
 
+<div class="columns fullscreen-columns">
+    <div class="column">
+        <section class="about-me">
+            <h1>Hi, I'm Kyle.</h1>
+            <p>I'm a creative technologist currently living in Arkansas. I mostly work on the web, but I'm also interested in visual design, 3D, game development and VR/AR&mdash;among other things.</p>
+            <p>This website is an archive of the work I've done for AT 5813 Creative Coding as part of the University of Oklahoma's <a href="https://ouarttechonline.com">Master of Art and Technology</a> program.</p>
+            <a class="about-me--link" href="/assignments">Assignments</a>
+            <a class="about-me--link" href="/projects">Projects</a>
+        </section>
+    </div>
+    <div class="column home-page-right">
+        <img class="full-img" alt="A picture of my virtual reality avatar in Facebook's Horizon Workrooms with a whiteboard in the background that reads, &quot;Hello, World&quot;" src="<?php echo get_template_directory_uri(); ?>/assets/images/vr.jpg">
+    </div>
+</div>

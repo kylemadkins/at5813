39bb5fb21e8f6cd2e1fa6f06eead781bee426861
Kyle Adkins <kyle.m.adkins@gmail.com>
2021-09-03 13:20:48 -0500

    Add social links

--- home-page.php
+++ home-page.php
@@ -17,6 +17,10 @@ get_header();
         </section>
     </div>
     <div class="column home-right-column">
+        <div class="social-links" data-aos="fade-up" data-aos-duration="1000">
+            <a href="https://twitter.com/kylemadkins"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitter.svg"></a>
+            <a href="https://www.linkedin.com/in/kyle-adkins-299012117"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/linkedin.svg"></a>
+        </div>
         <figure>
             <img class="full-img" alt="A picture of my virtual reality avatar in Facebook's Horizon Workrooms with a whiteboard in the background that reads, &quot;Hello, World&quot;" src="<?php echo get_template_directory_uri(); ?>/assets/images/vr.jpg">
             <figcaption>
